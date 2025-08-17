76b60601d070ebd5d05794ed77675c0e2727ed19
Alexis Szerman <alexszer1986@gmail.com>
2025-08-17 12:39:11 -0300

    second commit

--- src/components/GameInProgress.jsx
+++ src/components/GameInProgress.jsx
@@ -33,7 +33,7 @@ const GameInProgress = ({
 
         <div className="mb-8">
           <p className="text-xl font-medium text-gray-700 mb-4">
-            Pregunta {currentQuestionIndex + 1} de {/** total */} 
+            Pregunta {currentQuestionIndex + 1} de {20} 
           </p>
 
           {currentQuestion.type === "photo" && (
