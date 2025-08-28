43791604d338b41ad9435b1cd09469e6c7538af7
Alexis Szerman <alexszer1986@gmail.com>
2025-08-28 16:43:14 -0300

    modificado publicadores

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,205 +1,230 @@
 export const questions = [
-{
-        id: 1,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/einstein_romero.png?updatedAt=1755440465008',
-        question: "¿Quién es este hermano?",
-        correctAnswer: "Alberto Romero",
-        hint: "Pista: Alberto..."
-    },
-    {
-        id: 2,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/luke_herrero.png?updatedAt=1755440464940',
-        question: "¿Quién es este hermano?",
-        correctAnswer: "Lucas Herrera",
-        hint: "Pista: ...Herrera"
-    },
-    {
-        id: 3,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/estrella_mandarina.png?updatedAt=1755440464893',
-        question: "¿Quién es esta hermana?",
-        correctAnswer: "Estela Mandarini",
-        hint: "Pista: Estela..."
-    },
-    {
-        id: 4,
-        type: 'multiple-choice',
-        question: "¿Cuál es el apellido de Alexis?",
-        options: ["Szerman", "Sherman", "Serman"],
-        correctAnswer: "Szerman",
-        hint: "Pista: Ese Zeta"
-    },
-    {
-        id: 5,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/sandra_varela.png?updatedAt=1755440464851',
-        question: "¿Quién es esta hermana?",
-        correctAnswer: "Sandra Varela",
-        hint: "Pista: Sandra..."
-    },
-    {
-        id: 6,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/perro_pato.png?updatedAt=1755440464736',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Gustavo Perrupato",
-        hint: "Pista: Perru..."
-    },
-        {
-        id: 7,
-        type: 'multiple-choice',
-        question: "¿Qué representante de \"Babilonia la grande\" nació en el territorio de la congregación?",
-        options: ["Mahoma", "Papa Francisco", "Buda"],
-        correctAnswer: "Papa Francisco",
-        hint: "Pista: Era de San Lorenzo"
-    },
-            {
-        id: 8,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/pablo_burton.png?updatedAt=1755440464869',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Pablo Burtin",
-        hint: "Pista: Es el más alto"
-    },
-    {
-        id: 9,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/soledad_torres.png?updatedAt=1755440464747',
-        question: "¿Quien es esta hermana?",
-        correctAnswer: "Soledad Torres",
-        hint: "Pista: Soledad..."
-    },
-        {
-        id: 10,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/johnny_romano.png?updatedAt=1755440464678',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Jonatan Romano",
-        hint: "Pista: Jonatan..."
-    },
-    {
-        id: 11,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/oscar_fiorito.png?updatedAt=1755440464601',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Oscar Fioriti",
-        hint: "Pista: ... Fioriti"
-    },
-        {
-        id: 12,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/daniel_escobar.png?updatedAt=1755440464591',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Daniel Escobar",
-        hint: "Pista: ... Escobar"
-    },
-    {
-        id: 13,
-        type: 'multiple-choice',
-        question: "¿Cuántos ancianos hay en la congregación?",
-        options: ["7", "8", "9"],
-        correctAnswer: "9",
-        hint: "Pista: Cantidad de felicidades de Mateo 5:1-12"
-    },
-    {
-        id: 14,
-        type: 'multiple-choice',
-        question: "¿Qué hay clavado en la esquina de Recuero y Pedernera?",
-        options: ["Espada", "Ancla", "Hacha"],
-        correctAnswer: "Ancla",
-        hint: "Pista: Hebreos 6:19"
-    },
-        {
-        id: 15,
-        type: 'multiple-choice',
-        question: "¿Cuál es el verdadero nombre de la Plaza Flores?",
-        options: ["Plaza General Pueyrredon", "Plaza San José de Flores", "Plaza Basílica de Flores"],
-        correctAnswer: "Plaza General Pueyrredon",
-        hint: "Pista: No lleva flores en el nombre"
-    },
-            {
-        id: 16,
-        type: 'multiple-choice',
-        question: "¿De qué famoso comediante hay un busto en la Plaza de la Misericordia?",
-        options: ["Alberto Olmedo", "Carlos Chaplin", "Chespirito"],
-        correctAnswer: "Carlos Chaplin",
-        hint: "Pista: Sus películas eran mudas"
-    },
-                {
-        id: 17,
-        type: 'multiple-choice',
-        question: "¿Qué barrios abarca el territorio de la congregación?",
-        options: ["Flores y Caballito", "Flores, Caballito y Parque Chacabuco", "Flores, Floresta, Caballito y Parque Chacabuco"],
-        correctAnswer: "Flores, Floresta, Caballito y Parque Chacabuco",
-        hint: "Pista: Son cuatro"
-    },
-                    {
-        id: 18,
-        type: 'multiple-choice',
-        question: "¿Cuantos publicadores somos?",
-        options: ["91", "105", "89"],
-        correctAnswer: "91",
-        hint: "Pista: Uno mas de 90"
-    },
-                        {
-        id: 19,
-        type: 'multiple-choice',
-        question: "¿Cuantos precursores hay?",
-        options: ["25", "30", "35"],
-        correctAnswer: "25",
-        hint: "Pista: Uno mas de 90"
-    },
-    {
-        id: 20,
-        type: 'multiple-choice',
-        question: "¿Cuantos siervos ministeriales hay?",
-        options: ["10", "9", "8"],
-        correctAnswer: "8",
-        hint: "Pista: ¿Donde vivía el chavo?"
-    },
-    {
-        id: 21,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Monica%20Galati.png?updatedAt=1755636612279',
-        question: "¿Quien es esta hermana?",
-        correctAnswer: "Mónica Galati",
-        hint: "Pista: Mónica..."
-    },
-    {
-        id: 22,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Luz%20Gimenez.png?updatedAt=1755636588209',
-        question: "¿Quien es esta hermana?",
-        correctAnswer: "Luz Gimenez",
-        hint: "Pista: Luz..."
-    },
-    {
-        id: 23,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Reynaldo%20Jarro.png?updatedAt=1755636588193',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Reynaldo Jarro",
-        hint: "Pista: ...Jarro"
-    },
-    {
-        id: 24,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Agustina%20Farroni.png?updatedAt=1755636588197',
-        question: "¿Quien es esta hermana?",
-        correctAnswer: "Agustina Farroni",
-        hint: "Pista: ...Farroni"
-    },
-    {
-        id: 25,
-        type: 'photo',
-        image: 'https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Roberto%20Schettini.png?updatedAt=1755636588194',
-        question: "¿Quien es este hermano?",
-        correctAnswer: "Roberto Schettini",
-        hint: "Pista: ...Schettini"
-    },
+  {
+    id: 1,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/einstein_romero.png?updatedAt=1755440465008",
+    question: "¿Quién es este hermano?",
+    correctAnswer: "Alberto Romero",
+    hint: "Pista: Alberto...",
+  },
+  {
+    id: 2,
+    type: "multiple-choice",
+    question: "¿Cuál es el apellido de Alexis?",
+    options: ["Szerman", "Sherman", "Serman"],
+    correctAnswer: "Szerman",
+    hint: "Pista: Ese Zeta",
+  },
+  {
+    id: 3,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/luke_herrero.png?updatedAt=1755440464940",
+    question: "¿Quién es este hermano?",
+    correctAnswer: "Lucas Herrera",
+    hint: "Pista: ...Herrera",
+  },
+  {
+    id: 4,
+    type: "multiple-choice",
+    question:
+      '¿Qué representante de "Babilonia la grande" nació en el territorio de la congregación?',
+    options: ["Mahoma", "Papa Francisco", "Buda"],
+    correctAnswer: "Papa Francisco",
+    hint: "Pista: Era de San Lorenzo",
+  },
+  {
+    id: 5,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/estrella_mandarina.png?updatedAt=1755440464893",
+    question: "¿Quién es esta hermana?",
+    correctAnswer: "Estela Mandarini",
+    hint: "Pista: Estela...",
+  },
+  {
+    id: 6,
+    type: "multiple-choice",
+    question: "¿Cuántos ancianos hay en la congregación?",
+    options: ["7", "8", "9"],
+    correctAnswer: "9",
+    hint: "Pista: Cantidad de felicidades de Mateo 5:1-12",
+  },
+  {
+    id: 7,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/sandra_varela.png?updatedAt=1755440464851",
+    question: "¿Quién es esta hermana?",
+    correctAnswer: "Sandra Varela",
+    hint: "Pista: Sandra...",
+  },
+  {
+    id: 8,
+    type: "multiple-choice",
+    question: "¿Qué hay clavado en la esquina de Recuero y Pedernera?",
+    options: ["Espada", "Ancla", "Hacha"],
+    correctAnswer: "Ancla",
+    hint: "Pista: Hebreos 6:19",
+  },
+  {
+    id: 9,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/perro_pato.png?updatedAt=1755440464736",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Gustavo Perrupato",
+    hint: "Pista: Perru...",
+  },
+  {
+    id: 10,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/pablo_burton.png?updatedAt=1755440464869",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Pablo Burtin",
+    hint: "Pista: Es el más alto",
+  },
+  {
+    id: 9,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/soledad_torres.png?updatedAt=1755440464747",
+    question: "¿Quien es esta hermana?",
+    correctAnswer: "Soledad Torres",
+    hint: "Pista: Soledad...",
+  },
+  {
+    id: 10,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/johnny_romano.png?updatedAt=1755440464678",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Jonatan Romano",
+    hint: "Pista: Jonatan...",
+  },
+  {
+    id: 11,
+    type: "multiple-choice",
+    question: "¿Cuál es el verdadero nombre de la Plaza Flores?",
+    options: [
+      "Plaza General Pueyrredon",
+      "Plaza San José de Flores",
+      "Plaza Basílica de Flores",
+    ],
+    correctAnswer: "Plaza General Pueyrredon",
+    hint: "Pista: No lleva flores en el nombre",
+  },
+  {
+    id: 12,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/oscar_fiorito.png?updatedAt=1755440464601",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Oscar Fioriti",
+    hint: "Pista: ... Fioriti",
+  },
+  {
+    id: 13,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/daniel_escobar.png?updatedAt=1755440464591",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Daniel Escobar",
+    hint: "Pista: ... Escobar",
+  },
 
+  {
+    id: 14,
+    type: "multiple-choice",
+    question:
+      "¿De qué famoso comediante hay un busto en la Plaza de la Misericordia?",
+    options: ["Alberto Olmedo", "Carlos Chaplin", "Chespirito"],
+    correctAnswer: "Carlos Chaplin",
+    hint: "Pista: Sus películas eran mudas",
+  },
 
+  {
+    id: 18,
+    type: "multiple-choice",
+    question: "¿Cuantos publicadores somos?",
+    options: ["93", "105", "89"],
+    correctAnswer: "93",
+    hint: "Pista: Año del anuario donde sale Dinamarca, Honduras y Malasia",
+  },
 
+  {
+    id: 21,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Monica%20Galati.png?updatedAt=1755636612279",
+    question: "¿Quien es esta hermana?",
+    correctAnswer: "Mónica Galati",
+    hint: "Pista: Mónica...",
+  },
+  {
+    id: 17,
+    type: "multiple-choice",
+    question: "¿Qué barrios abarca el territorio de la congregación?",
+    options: [
+      "Flores y Caballito",
+      "Flores, Caballito y Parque Chacabuco",
+      "Flores, Floresta, Caballito y Parque Chacabuco",
+    ],
+    correctAnswer: "Flores, Floresta, Caballito y Parque Chacabuco",
+    hint: "Pista: FFCP",
+  },
+  {
+    id: 22,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Luz%20Gimenez.png?updatedAt=1755636588209",
+    question: "¿Quien es esta hermana?",
+    correctAnswer: "Luz Gimenez",
+    hint: "Pista: Luz...",
+  },
+  {
+    id: 20,
+    type: "multiple-choice",
+    question: "¿Cuantos siervos ministeriales hay?",
+    options: ["10", "9", "8"],
+    correctAnswer: "8",
+    hint: "Pista: ¿Donde vivía el chavo?",
+  },
+  {
+    id: 23,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Reynaldo%20Jarro.png?updatedAt=1755636588193",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Reynaldo Jarro",
+    hint: "Pista: ...Jarro",
+  },
+  {
+    id: 24,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Agustina%20Farroni.png?updatedAt=1755636588197",
+    question: "¿Quien es esta hermana?",
+    correctAnswer: "Agustina Farroni",
+    hint: "Pista: ...Farroni",
+  },
+  {
+    id: 19,
+    type: "multiple-choice",
+    question: "¿Cuantos precursores hay?",
+    options: ["25", "30", "35"],
+    correctAnswer: "25",
+    hint: "Pista: Uno mas de 90",
+  },
+  {
+    id: 25,
+    type: "photo",
+    image:
+      "https://ik.imagekit.io/7nyxivlfm/conociendo-mise/Roberto%20Schettini.png?updatedAt=1755636588194",
+    question: "¿Quien es este hermano?",
+    correctAnswer: "Roberto Schettini",
+    hint: "Pista: ...Schettini",
+  },
 ];
